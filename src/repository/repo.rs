d2d1788fe1e47fb5853fe465dd54926219c804ea
jan Michal <michal@tar.black>
2022-01-30 19:56:36 +0000

    testing again

--- src/repository/repo.rs
+++ src/repository/repo.rs
@@ -27,7 +27,7 @@ pub fn generate() {
     let files = format!("{}.files", &name);
 
     Command::new("bash")
-        .args(&["-c", &format!("repo-add {}.tar.gz *.pkg.tar.xz *.pkg.tar.zst", db)])
+        .args(&["-c", &format!("repo-add {}.tar.gz *.pkg.tar.zst; repo-add {}.tar.gz *.pkg.tar.xz", db)])
         .spawn()
         .unwrap()
         .wait()
