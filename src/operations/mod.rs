1d474bf6f507196a8b400716011d36e42f78c066
Michal <michal@tar.black>
2022-07-23 04:32:30 +0100

    an fmt for the night

--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -2,14 +2,14 @@ pub use build::*;
 pub use clean::*;
 pub use clone::*;
 pub use config::*;
+pub use info::*;
 pub use prune::*;
 pub use pull::*;
-pub use info::*;
 
 mod build;
 mod clean;
 mod clone;
 mod config;
+mod info;
 mod prune;
 mod pull;
-mod info;
